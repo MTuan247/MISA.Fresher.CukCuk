8ec2e86f39042200fbde3ab971042dfc168f8e78
MTuan247 <mtuan2472000@gmail.com>
2021-07-13 21:54:46 +0700

    bind data to form

--- src/js/common/crud.js
+++ src/js/common/crud.js
@@ -5,10 +5,11 @@ var $ = require('jquery')
 export function getDataById(id) {
     axios.get('http://cukcuk.manhnv.net/v1/Employees/' + id )
     .then((response) => {
+        let res = response.data
         $('.info-form .field-label').each((index, item) => {
             let fieldName = $(item).attr('fieldName')
             let fieldType = $(item).attr('fieldType')
-            let value =response[fieldName]
+            let value =res[fieldName]
             value = formatDataInput(value, fieldType)
             
             $(item).find('input').val(value)
